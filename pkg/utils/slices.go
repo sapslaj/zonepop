e21c56f67fd5c689a840ee02fdddc7afa878d42d
sapslaj <saps.laj@gmail.com>
2023-03-13 23:10:58 -0400

    Add FilterErr and MapErr slice functions and refactor other tests slightly
    New functions FilterErr and MapErr which do what they say on the tin but
    with error handling. For those cases where it needs to be a bit more
    robust. Also makes the tests use `t.Run` and switches the comparison to
    `go-cmp` because `reflect.DeepEqual` has unexpected behavior! Also
    go-cmp's output is way nicer.

--- pkg/utils/slices.go
+++ pkg/utils/slices.go
@@ -9,8 +9,28 @@ func Reversed[V any](source []V) []V {
 	return result
 }
 
+// FilterErr returns a new slice with f() applied to each element to determine
+// whether that element should be included in the new slice. Unlike Filter, the
+// function should return (bool, error) and if an error is returned, FilterErr
+// immediately bubbles the error up instead of continuing with the loop.
+func FilterErr[V any](f func(V) (bool, error), source []V) ([]V, error) {
+	result := make([]V, 0)
+	for i := 0; i < len(source); i++ {
+		pass, err := f(source[i])
+		if err != nil {
+			return result, err
+		}
+		if pass {
+			result = append(result, source[i])
+		}
+	}
+	return result, nil
+}
+
 // Filter returns a new slice with f() applied to each element to determine
-// whether that element should be included in the new slice.
+// whether that element should be included in the new slice. Unlike FilterErr,
+// the function should only return a bool depending on whether it should be
+// filtered through or not.
 func Filter[V any](f func(V) bool, source []V) []V {
 	result := make([]V, 0)
 	for i := 0; i < len(source); i++ {
@@ -21,7 +41,23 @@ func Filter[V any](f func(V) bool, source []V) []V {
 	return result
 }
 
-// Map calls function f for each item in source slice.
+// MapErr calls function f for each item in source slice. Unlike Map, the
+// function should return (B, error) and if an error is returned, MapErr
+// immatately bubbles the error up instead of continuing the loop.
+func MapErr[A any, B any](f func(A) (B, error), source []A) ([]B, error) {
+	result := make([]B, 0)
+	for i := 0; i < len(source); i++ {
+		mapped, err := f(source[i])
+		if err != nil {
+			return result, err
+		}
+		result = append(result, mapped)
+	}
+	return result, nil
+}
+
+// Map calls function f for each item in source slice. Unlike MapErr, the
+// function should only return the B type.
 func Map[A any, B any](f func(A) B, source []A) []B {
 	result := make([]B, 0)
 	for i := 0; i < len(source); i++ {
--- pkg/utils/slices_test.go
+++ pkg/utils/slices_test.go
@@ -1,9 +1,11 @@
 package utils
 
 import (
+	"errors"
 	"fmt"
-	"reflect"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestReversed(t *testing.T) {
@@ -22,10 +24,47 @@ func TestReversed(t *testing.T) {
 	}
 
 	for desc, tc := range tests {
-		got := Reversed(tc.input)
-		if !reflect.DeepEqual(tc.expected, got) {
-			t.Errorf("%s: expected %q, got %q", desc, tc.expected, got)
-		}
+		t.Run(desc, func(t *testing.T) {
+			got := Reversed(tc.input)
+			if diff := cmp.Diff(tc.expected, got); diff != "" {
+				t.Fatalf("%s: mismatch:\n%s", desc, diff)
+			}
+		})
+	}
+}
+
+func TestFilterErr(t *testing.T) {
+	tests := map[string]struct {
+		input    []string
+		expected []string
+		err      error
+		f        func(string) (bool, error)
+	}{
+		"filter with no error": {
+			input:    []string{"a", "b", "c", "a", "b", "c"},
+			expected: []string{"b", "c", "b", "c"},
+			f:        func(s string) (bool, error) { return s != "a", nil },
+		},
+		"filter with error": {
+			input:    []string{"a", "c", "b"},
+			expected: []string{},
+			err:      errors.New("dummy error"),
+		},
+	}
+
+	for desc, tc := range tests {
+		t.Run(desc, func(t *testing.T) {
+			if tc.f == nil {
+				tc.f = func(s string) (bool, error) { return false, tc.err }
+			}
+			got, err := FilterErr(tc.f, tc.input)
+			if !errors.Is(err, tc.err) {
+				t.Fatalf("%s: expected error %v, got %v", desc, tc.err, err)
+			}
+			if diff := cmp.Diff(tc.expected, got); diff != "" {
+				t.Fatalf("%s: mismatch:\n%s", desc, diff)
+			}
+		})
 	}
 }
 
@@ -43,10 +82,47 @@ func TestFilter(t *testing.T) {
 	}
 
 	for desc, tc := range tests {
-		got := Filter(tc.f, tc.input)
-		if !reflect.DeepEqual(tc.expected, got) {
-			t.Errorf("%s: expected %q, got %q", desc, tc.expected, got)
-		}
+		t.Run(desc, func(t *testing.T) {
+			got := Filter(tc.f, tc.input)
+			if diff := cmp.Diff(tc.expected, got); diff != "" {
+				t.Fatalf("%s: mismatch:\n%s", desc, diff)
+			}
+		})
+	}
+}
+
+func TestMapErr(t *testing.T) {
+	tests := map[string]struct {
+		input    []string
+		expected []string
+		err      error
+		f        func(string) (string, error)
+	}{
+		"func with no error": {
+			input:    []string{"69", "420"},
+			expected: []string{"69", "420"},
+			f:        func(s string) (string, error) { return s, nil },
+		},
+		"func with error": {
+			input:    []string{"69", "420"},
+			expected: []string{},
+			err:      errors.New("dummy error"),
+		},
+	}
+
+	for desc, tc := range tests {
+		t.Run(desc, func(t *testing.T) {
+			if tc.f == nil {
+				tc.f = func(s string) (string, error) { return s, tc.err }
+			}
+			got, err := MapErr(tc.f, tc.input)
+			if !errors.Is(err, tc.err) {
+				t.Fatalf("%s: expected error %v, got %v", desc, tc.err, err)
+			}
+			if diff := cmp.Diff(tc.expected, got); diff != "" {
+				t.Fatalf("%s: mismatch:\n%s", desc, diff)
+			}
+		})
 	}
 }
 
@@ -64,9 +140,11 @@ func TestMap(t *testing.T) {
 	}
 
 	for desc, tc := range tests {
-		got := Map(tc.f, tc.input)
-		if !reflect.DeepEqual(tc.expected, got) {
-			t.Errorf("%s: expected %q, got %q", desc, tc.expected, got)
-		}
+		t.Run(desc, func(t *testing.T) {
+			got := Map(tc.f, tc.input)
+			if diff := cmp.Diff(tc.expected, got); diff != "" {
+				t.Fatalf("%s: mismatch:\n%s", desc, diff)
+			}
+		})
 	}
 }
